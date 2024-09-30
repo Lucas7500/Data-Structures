3f034d0846b444f0ce30196f8e1acf231fcadd03
Lucas lglesias <lucas.lglesias@inventsoftware.com.br>
2024-09-29 22:14:10 -0300

    feat: adding traversal methods

--- Binary-Trees/Binary-Tree/Models/BinaryTree.cs
+++ Binary-Trees/Binary-Tree/Models/BinaryTree.cs
@@ -5,20 +5,109 @@
         public Node? Root { get; protected set; }
         public int Height { get => GetMaximumDepth(Root); }
 
-        //protected IEnumerable<string> PreOrderTraversal()
+        public IEnumerable<int> PreOrderTraversal()
+        {
+            return GetPreOrderTraversal(Root);
+        }
+        
+        public IEnumerable<int> InOrderTraversal()
+        {
+            return GetInOrderTraversal(Root);
+        }
+
+        public IEnumerable<int> PostOrderTraversal()
+        {
+            return GetPostOrderTraversal(Root);
+        }
+
+        //public bool Contains(int data)
         //{
         //    if (Root is null)
+        //        return false;
+
+        //    if (Root.Data == data)
         //    {
-        //        return [];
+        //        return true;
         //    }
-
-
-        //    return GetPreOrderTraversal(Root);
+        //    else if (Root.Data > data && Root.Left is not null)
+        //    {
+        //        return Contains(Root.Left.Data);
+        //    }
+        //    else if (Root.Data < data && Root.Right is not null)
+        //    {
+        //        return Contains(Root.Right.Data);
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
         //}
 
-        //private List<string> GetPreOrderTraversal(Node root)
-        //{
-        //}
+        private static List<int> GetPreOrderTraversal(Node? root)
+        {
+            var traversal = new List<int>();
+
+            if (root is not null)
+            {
+                traversal.Add(root.Data);
+
+                if (root.Left is not null)
+                {
+                    traversal.AddRange(GetPreOrderTraversal(root.Left));
+                }
+
+                if (root.Right is not null)
+                {
+                    traversal.AddRange(GetPreOrderTraversal(root.Right));
+                }
+            }
+
+            return traversal;
+        }
+
+        private static List<int> GetInOrderTraversal(Node? root)
+        {
+            var traversal = new List<int>();
+
+            if (root is not null)
+            {
+                if (root.Left is not null)
+                {
+                    traversal.AddRange(GetInOrderTraversal(root.Left));
+                }
+
+                traversal.Add(root.Data);
+
+                if (root.Right is not null)
+                {
+                    traversal.AddRange(GetInOrderTraversal(root.Right));
+                }
+            }
+
+            return traversal;
+        }
+
+        private static List<int> GetPostOrderTraversal(Node? root)
+        {
+            var traversal = new List<int>();
+
+            if (root is not null)
+            {
+                if (root.Left is not null)
+                {
+                    traversal.AddRange(GetPostOrderTraversal(root.Left));
+                }
+
+                if (root.Right is not null)
+                {
+                    traversal.AddRange(GetPostOrderTraversal(root.Right));
+                }
+
+                traversal.Add(root.Data);
+            }
+
+            return traversal;
+        }
 
         private static int GetMaximumDepth(Node? root)
         {
--- Binary-Trees/Binary-Tree/Program.cs
+++ Binary-Trees/Binary-Tree/Program.cs
@@ -3,11 +3,42 @@
 var bt = new BinarySearchTree();
 
 bt.Insert(5);
-bt.Insert(1);
-bt.Insert(2);
 bt.Insert(3);
+bt.Insert(2);
 bt.Insert(4);
+bt.Insert(5);
 bt.Insert(6);
-bt.Insert(7);
-bt.Insert(8);
-var a  = new BinarySearchTree();
+
+Console.WriteLine("Pre Order Traversal: ");
+foreach (var item in bt.PreOrderTraversal())
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine('\n');
+
+Console.WriteLine("In Order Traversal: ");
+foreach (var item in bt.InOrderTraversal())
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine('\n');
+
+Console.WriteLine("Post Order Traversal: ");
+foreach (var item in bt.PostOrderTraversal())
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine('\n');
+
+//Console.WriteLine(bt.Contains(5));
+//Console.WriteLine(bt.Contains(3));
+//Console.WriteLine(bt.Contains(2));
+//Console.WriteLine(bt.Contains(4));
+//Console.WriteLine(bt.Contains(5));
+//Console.WriteLine(bt.Contains(6));
+//Console.WriteLine(bt.Contains(10));
+//Console.WriteLine(bt.Contains(12));
+//Console.WriteLine(bt.Contains(0));
